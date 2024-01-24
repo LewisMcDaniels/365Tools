703e9eb9e7aeed147012436d8efbf28d5b61e1e8
Lewis.McDaniels <LewisMcDaniels.lm@gmail.com>
2024-01-24 10:32:45 +0000

    Adding generic names to scipts, adding module installers. Added instructions to AddSMTPs.ps1 file.

--- Exchange/AddMultipleSMTPStoMailboxes-master/AddSMTPs.ps1
+++ Exchange/AddMultipleSMTPStoMailboxes-master/AddSMTPs.ps1
@@ -1,22 +1,23 @@
-Import-Module ExchangeOnlineManagement
-Connect-ExchangeOnline -UserPrincipalName Bittitan@williammartin.onmicrosoft.com
+# Install required modules
+if (-not (Get-Module -Name ExchangeOnlineManagement -ListAvailable)) {
+    Install-Module -Name ExchangeOnlineManagement -Force
+}
 
-#One csv for each domain, easier and cleaner
-$SMTP1 = "C:\Scripts\cedrec.csv"
-$SMTP2 = "C:\Scripts\environmentalcouk.csv"
-$SMTP3 = "C:\Scripts\healthandsafetycouk.csv"
-$SMTP4 = "C:\Scripts\cedreccouk.csv"
-$SMTP5 = "C:\Scripts\cedreceu.csv"
-$SMTP6 = "C:\Scripts\cedrecinfo.csv"
-$SMTP7 = "C:\Scripts\cedrecnet.csv"
-$SMTP8 = "C:\Scripts\barbourehscom.csv"
+# Connect to Exchange Online
+Connect-ExchangeOnline
+
+
+# One csv for each domain, easier and cleaner
+# Use template.csv to create your own csv files.
+$SMTP1 = "C:\Scripts\Domain1.csv"
+$SMTP2 = "C:\Scripts\Domain2.csv"
+$SMTP3 = "C:\Scripts\Domain3.csv"
+$SMTP4 = "C:\Scripts\Domain4.csv"
+
 
 #Import from CSV
 Import-CSV $SMTP1 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
 Import-CSV $SMTP2 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
 Import-CSV $SMTP3 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
 Import-CSV $SMTP4 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
-Import-CSV $SMTP5 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
-Import-CSV $SMTP6 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
-Import-CSV $SMTP7 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
-Import-CSV $SMTP8 | ForEach-Object {Set-Mailbox $_.Mailbox -EmailAddresses @{add=$_.NewEmailAddress}}
+
--- Exchange/AddMultipleSMTPStoMailboxes-master/GetSMTPS.ps1
+++ Exchange/AddMultipleSMTPStoMailboxes-master/GetSMTPS.ps1
@@ -1,3 +1,13 @@
+# Install required modules
+if (-not (Get-Module -Name ExchangeOnlineManagement -ListAvailable)) {
+    Install-Module -Name ExchangeOnlineManagement -Force
+}
+
+# Connect to Exchange Online
+Connect-ExchangeOnline
+
+
+
 $Users = "C:\scripts\users.csv"
 Import-CSV $Users| ForEach-Object {Get-Mailbox -Identity $_.Users | Select-Object Alias,@{Name=”EmailAddresses”;Expression={$_.EmailAddresses |Where-Object {$_ -LIKE “SMTP:*”}}}} | Format-List
 
