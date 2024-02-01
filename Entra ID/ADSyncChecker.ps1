9e5d862aff26dc69e086f50318d43fdfe1007c51
lewismcdaniels <lewismcdaniels.lm@gmail.com>
2024-02-01 13:17:25 +0000

    Created AD Sync Checker tool. Tested working.

---
+++ Entra ID/ADSyncChecker.ps1
@@ -0,0 +1,14 @@
+if (-not (Get-Module -Name MSOnline -ListAvailable)) {
+    Install-Module -Name MSOnline -Force
+}
+Import-Module -Name MSOnline -Force
+Connect-MsolService
+
+$companyInfo = Get-MsolCompanyInformation
+$companyname = $companyInfo | Select-Object -ExpandProperty DisplayName
+$lastsync = $companyInfo | Select-Object -ExpandProperty LastDirSyncTime
+$lastpasswdsync = $companyInfo | Select-Object -ExpandProperty LastPasswordSyncTime
+
+Write-Host "Company Name: $companyname"
+Write-Host "Last DirSync: $lastsync"
+Write-Host "Last Password Sync: $lastpasswdsync"
