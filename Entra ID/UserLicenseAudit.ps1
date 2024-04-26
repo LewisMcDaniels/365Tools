23dbecd64fdba4f2cbf888d4d398a5dc17bceea4
LewisMcDaniels <67421537+LewisMcDaniels@users.noreply.github.com>
2024-04-26 17:21:58 +0100

    UserLicenseAudit created, needs testing.

---
+++ Entra ID/UserLicenseAudit.ps1
@@ -0,0 +1,23 @@
+# Install required modules if not already installed
+$requiredModules = @("Microsoft.Graph.users")
+
+foreach ($module in $requiredModules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module -Name $module -Force
+    }
+}
+
+# Import the MS Graph module
+Import-Module Microsoft.Graph.users
+
+# Connect to MS Graph
+Connect-MgGraph -Scopes "User.Read.All" -NoWelcome
+
+# Get all disabled users with licenses still applied
+$LicUsers = Get-MgUser  -All | 
+Where-Object {$_.AssignedLicenses.Count -gt 0} | 
+Select-Object -Property UserPrincipalName, DisplayName, AssignedLicenses
+
+# Output the disabled users with licenses to a CSV file
+Write-Host "Exporting disabled users with licenses to CSV file, see C:\UserLicenseAudit.csv" -ForegroundColor Green
+$LicUsers | Export-Csv -Path "C:\UserLicenseAudit.csv" -NoTypeInformation
